20c04441defbd9c69685adb00de131a8cde8ff4a
Kerem Nalbant <kerem.nalbant@hotmail.com>
2023-07-04 04:06:55 +0300

    Feature: Get guess endpoint to return on page refresh

--- modules/guess/router.py
+++ modules/guess/router.py
@@ -14,3 +14,8 @@ guess_service = GuessService()
 @router.post("", status_code=status.HTTP_201_CREATED)
 async def guess(data: GuessInput, current_user: User = Depends(get_current_user)):
     return await guess_service.guess(data.guess, game_manager.id, current_user)
+
+
+@router.get("", status_code=status.HTTP_200_OK)
+async def guess(current_user: User = Depends(get_current_user)):
+    return await guess_service.get_by_game_id(game_manager.id, current_user)
